7ef57763d02ead04c043148af9f60e244d05741b
Syrkery <e3075589k@gmail.com>
2024-11-26 23:38:22 +0300

    Добавил переход в новое окно при успешной регистрации или входе

--- cost accounting system.py
+++ cost accounting system.py
@@ -36,28 +36,39 @@ class Login(QMainWindow):
     def cont(self):
         con = sqlite3.connect('cost accounting system.sqlite')
         cur = con.cursor()
-        u_name_check = cur.execute("""Select username FROM Users""").fetchall()
+        uname = False
+        mail = False
+        passw = False
+        u_name_check = cur.execute("""SELECT username FROM Users""").fetchall()
         email_check = cur.execute("""SELECT email FROM Users""").fetchall()
         pasw_ckeck = cur.execute("""SELECT password FROM Users""").fetchall()
         username = self.user_name.toPlainText()
         e_mail = self.email.toPlainText()
         pasw = self.password.text()
 
-        if username in u_name_check:
-            print(1)
+        if (username,) in u_name_check:
+            uname = True
         else:
             self.problems.append('Invalid username')
             self.user_name.clear()
-        if e_mail in email_check:
-            print(1)
+        if (e_mail,) in email_check:
+            mail = True
         else:
             self.problems.append('Invalid e-mail')
             self.email.clear()
-        if pasw in pasw_ckeck:
-            print(1)
+        if (pasw,) in pasw_ckeck:
+            passw = True
         else:
             self.problems.append('Invalid password')
             self.password.clear()
+        if uname and mail and passw:
+            self.problems.append('OK!')
+            self.open_main_window()
+
+    def open_main_window(self):
+        self.main_window = Main()
+        self.main_window.show()
+        self.close()
 
     def go_back(self):
         self.parent.show()
@@ -74,6 +85,7 @@ class Register(QMainWindow):
         self.email = self.findChild(QTextEdit, 'email')
         self.password = self.findChild(QLineEdit, 'password')
         self.password_repeat = self.findChild(QLineEdit, 'password_repeat')
+        self.problems = self.findChild(QTextBrowser, 'problems')
         self.OK.clicked.connect(self.cont)
 
     def cont(self):
@@ -81,7 +93,7 @@ class Register(QMainWindow):
         cur = con.cursor()
         usern = False
         mail = False
-        pasw = False
+        passw = False
         u_name_check = cur.execute("""SELECT username FROM Users""").fetchall()
         email_check = cur.execute("""SELECT email FROM Users""").fetchall()
         pasw_ckeck = cur.execute("""SELECT password FROM Users""").fetchall()
@@ -90,33 +102,45 @@ class Register(QMainWindow):
         pasw = self.password.text()
         pasw_rep = self.password_repeat.text()
 
-        if username not in u_name_check:
+        if (username,) not in u_name_check:
             usern = True
         else:
             self.problems.append('Invalid username')
             self.user_name.clear()
-        if e_mail not in email_check:
+        if (e_mail,) not in email_check:
             mail = True
         else:
             self.problems.append('Invalid e-mail')
             self.email.clear()
-        if pasw not in pasw_ckeck and pasw == pasw_rep and len(pasw) >= 7:
-            pasw = True
+        if (pasw,) not in pasw_ckeck and pasw == pasw_rep and len(pasw) >= 7:
+            passw = True
         else:
             self.problems.append('Invalid password')
             self.password.clear()
             if pasw != pasw_rep:
                 self.password_repeat.clear()
 
-        if usern and mail and pasw:
+        if usern and mail and passw:
             cur.execute("""INSERT INTO Users(username, password, email, created_at) VALUES(?, ?, ?, ?)""",
                         (username, pasw, e_mail, ''))
             con.commit()
+            self.problems.append('OK!')
+            self.open_main_window()
+
+    def open_main_window(self):
+        self.main_window = Main()
+        self.main_window.show()
+        self.close()
 
     def go_back(self):
         self.parent.show()
         self.close()
 
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Main.ui', self)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
